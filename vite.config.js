a4ee5aea849b1ed4338ba3a7e50bc4d314b6e3b1
Sandro-it <kozachenkooleksandr@gmail.com>
2024-09-19 11:33:36 +0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Various_elements/", // Додаємо базовий шлях для GitHub Pages
   build: {
     sourcemap: true,
   },
